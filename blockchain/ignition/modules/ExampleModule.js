659b23318227d96f466a5e7b9d21e1ae548043d3
Anthony Spedaliere <avspedaliere@gmail.com>
2025-04-25 17:47:06 -0700

    set up basic dependencies

---
+++ blockchain/ignition/modules/ExampleModule.js
@@ -0,0 +1,17 @@
+// const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
+
+// const VendingMachineModule = buildModule("VendingMachineModule", (m) => {
+// Deploy the VendingMachine contract
+//   const vendingMachine = m.contract("VendingMachine");
+
+// Example of adding a product after deployment (if desired)
+// const productId = 1;
+// const quantity = 10;
+// const price = ethers.utils.parseEther("0.01");
+// const addProduct = m.call(vendingMachine, "addProduct", [productId, quantity, price]);
+
+// Return the deployed contract
+//   return { vendingMachine };
+// });
+
+// module.exports = VendingMachineModule;
