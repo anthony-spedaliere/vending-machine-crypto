08501dd3df27d13fc76c1e710c7f6903c7c97fab
Anthony Spedaliere <avspedaliere@gmail.com>
2025-04-29 09:44:48 -0700

    Installed hardhat-chai-matchers and hardhat-ethers libraries in project

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,5 +1,7 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-ignition");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomicfoundation/hardhat-ethers");
 
 module.exports = {
   solidity: "0.8.28",
