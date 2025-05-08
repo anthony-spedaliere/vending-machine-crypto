b76c8c45502adeab7872b5a4a1df849c50c9b116
Anthony Spedaliere <avspedaliere@gmail.com>
2025-05-07 17:20:27 -0700

    set up starter code in VendingMachine.test.js

---
+++ blockchain/test/VendingMachine.test.js
@@ -0,0 +1,11 @@
+/**
+ * @title VendingMachine Test Suite
+ * @author Anthony Spedaliere
+ * @notice Tests for the VendingMachine ERC721 token implementation
+ * @dev Tests cover
+ */
+
+const { expect } = require("chai");
+const { ethers, upgrades } = require("hardhat");
+
+describe("VendingMachine (UUPS)", function () {});
